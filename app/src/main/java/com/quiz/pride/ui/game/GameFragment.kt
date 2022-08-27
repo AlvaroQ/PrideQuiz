87e3adf7ca488317d3e4ed0f9dd3d2b3e68bfdbe
Quintana <alvaroqp@gmail.com>
2022-08-27 08:25:58 +0200

    version name update and shor rewards each 9 stages

--- app/src/main/java/com/quiz/pride/ui/game/GameFragment.kt
+++ app/src/main/java/com/quiz/pride/ui/game/GameFragment.kt
@@ -421,7 +421,7 @@ class GameFragment : Fragment() {
                     gameViewModel.navigateToResult(points.toString())
                 } else {
                     gameViewModel.generateNewStage()
-                    if(stage != 0 && stage % 10 == 0) gameViewModel.showRewardedAd()
+                    if(stage != 0 && stage % 9 == 0) gameViewModel.showRewardedAd()
                 }
             }
         }
